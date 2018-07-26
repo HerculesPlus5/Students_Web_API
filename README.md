116874d38e2790f71df81c50ea5126bd6780ca60
Yong-Hwan Chee <vc.yonghwan.chee@lowes.com>
2018-07-26 08:11:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Students Web API With ASP.NET Core 2
+## Description:
+Students web api is very simple application that has basic functions.
+This service will be consumed by 
+### 1. RestSharp 
+### 2. HttpClient in C#
+
+## How to run it
+1. Download Students api, update your server name in StartUp.cs file. 
+2. Run the application.
+(This will create Students db)
+
+## How to see the endpoings in Swagger
+In a browser, type http://localhost:7496/swagger/ in the adress bar and hit Enter key.
+ 
+![swagger1](https://user-images.githubusercontent.com/25840262/39701405-da39a79e-51b5-11e8-8355-c569154f3373.PNG)
+![swagger2](https://user-images.githubusercontent.com/25840262/39701410-dd9f7ae4-51b5-11e8-9995-00cfc6a0a542.PNG)
+
---
+++ Student_WebAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Student_WebAPI.Data;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Student_WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<StudentsDbContext>(option 
+                => option.UseSqlServer(@"Server=NB0067-PC0J9BHG;Database=StudentDb;Integrated Security=True; Trusted_Connection=True"));
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info() { Title = "Students Api", Version = "v1" }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, StudentsDbContext studentsDbContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api for Students"));
+            app.UseMvc();
+            // if db doesn't exist, create one
+            studentsDbContext.Database.EnsureCreated();
+        }
+    }
+}
